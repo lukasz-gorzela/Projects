4210395c9f8cf7344632c2ba3cd03955de5b5752
lukasz-gorzela <lukasz.gorzelaa@gmail.com>
2024-11-04 12:25:34 +0100

    Add files via upload

---
+++ Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonChooseColor = new System.Windows.Forms.Button();
+            this.buttonEraser = new System.Windows.Forms.Button();
+            this.buttonUndo = new System.Windows.Forms.Button();
+            this.buttonRedo = new System.Windows.Forms.Button();
+            this.buttonCut = new System.Windows.Forms.Button();
+            this.buttonPaste = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonCutSelection = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonChooseColor
+            // 
+            resources.ApplyResources(this.buttonChooseColor, "buttonChooseColor");
+            this.buttonChooseColor.Name = "buttonChooseColor";
+            this.buttonChooseColor.UseVisualStyleBackColor = true;
+            this.buttonChooseColor.Click += new System.EventHandler(this.buttonChooseColor_Click);
+            // 
+            // buttonEraser
+            // 
+            resources.ApplyResources(this.buttonEraser, "buttonEraser");
+            this.buttonEraser.Name = "buttonEraser";
+            this.buttonEraser.UseVisualStyleBackColor = true;
+            this.buttonEraser.Click += new System.EventHandler(this.buttonEraser_Click);
+            // 
+            // buttonUndo
+            // 
+            resources.ApplyResources(this.buttonUndo, "buttonUndo");
+            this.buttonUndo.Name = "buttonUndo";
+            this.buttonUndo.UseVisualStyleBackColor = true;
+            this.buttonUndo.Click += new System.EventHandler(this.buttonUndo_Click);
+            // 
+            // buttonRedo
+            // 
+            resources.ApplyResources(this.buttonRedo, "buttonRedo");
+            this.buttonRedo.Name = "buttonRedo";
+            this.buttonRedo.UseVisualStyleBackColor = true;
+            this.buttonRedo.Click += new System.EventHandler(this.buttonRedo_Click);
+            // 
+            // buttonCut
+            // 
+            resources.ApplyResources(this.buttonCut, "buttonCut");
+            this.buttonCut.Name = "buttonCut";
+            this.buttonCut.UseVisualStyleBackColor = true;
+            this.buttonCut.Click += new System.EventHandler(this.buttonCut_Click);
+            // 
+            // buttonPaste
+            // 
+            resources.ApplyResources(this.buttonPaste, "buttonPaste");
+            this.buttonPaste.Name = "buttonPaste";
+            this.buttonPaste.UseVisualStyleBackColor = true;
+            this.buttonPaste.Click += new System.EventHandler(this.buttonPaste_Click);
+            // 
+            // buttonCopy
+            // 
+            resources.ApplyResources(this.buttonCopy, "buttonCopy");
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonCutSelection
+            // 
+            resources.ApplyResources(this.buttonCutSelection, "buttonCutSelection");
+            this.buttonCutSelection.Name = "buttonCutSelection";
+            this.buttonCutSelection.UseVisualStyleBackColor = true;
+            this.buttonCutSelection.Click += new System.EventHandler(this.buttonCutSelection_Click);
+            // 
+            // buttonSave
+            // 
+            resources.ApplyResources(this.buttonSave, "buttonSave");
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            resources.ApplyResources(this.buttonOpen, "buttonOpen");
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonCutSelection);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonPaste);
+            this.Controls.Add(this.buttonCut);
+            this.Controls.Add(this.buttonRedo);
+            this.Controls.Add(this.buttonUndo);
+            this.Controls.Add(this.buttonEraser);
+            this.Controls.Add(this.buttonChooseColor);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonChooseColor;
+        private System.Windows.Forms.Button buttonEraser;
+        private System.Windows.Forms.Button buttonUndo;
+        private System.Windows.Forms.Button buttonRedo;
+        private System.Windows.Forms.Button buttonCut;
+        private System.Windows.Forms.Button buttonPaste;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonCutSelection;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+    }
+}
+
+
