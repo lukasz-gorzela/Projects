d9e3a3e7d22979599de882acfc99ebba07923937
lukasz-gorzela <lukasz.gorzelaa@gmail.com>
2024-11-04 12:21:11 +0100

    Add files via upload

---
+++ NotesManager/NotesManager/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace NotesManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reminderDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.setReminderButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.categoryFilterComboBox = new System.Windows.Forms.ComboBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.contentTextBox = new System.Windows.Forms.TextBox();
+            this.notesListView = new System.Windows.Forms.ListView();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.filterButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DeleteNoteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reminderDateTimePicker
+            // 
+            this.reminderDateTimePicker.Location = new System.Drawing.Point(897, 37);
+            this.reminderDateTimePicker.Name = "reminderDateTimePicker";
+            this.reminderDateTimePicker.Size = new System.Drawing.Size(377, 31);
+            this.reminderDateTimePicker.TabIndex = 29;
+            this.reminderDateTimePicker.ValueChanged += new System.EventHandler(this.reminderDateTimePicker_ValueChanged);
+            // 
+            // setReminderButton
+            // 
+            this.setReminderButton.Location = new System.Drawing.Point(897, 76);
+            this.setReminderButton.Name = "setReminderButton";
+            this.setReminderButton.Size = new System.Drawing.Size(377, 108);
+            this.setReminderButton.TabIndex = 28;
+            this.setReminderButton.Text = "Ustaw Przypomnienie";
+            this.setReminderButton.UseVisualStyleBackColor = true;
+            this.setReminderButton.Click += new System.EventHandler(this.setReminderButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(645, 419);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(227, 98);
+            this.searchButton.TabIndex = 27;
+            this.searchButton.Text = "Szukaj Notatki";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // categoryFilterComboBox
+            // 
+            this.categoryFilterComboBox.FormattingEnabled = true;
+            this.categoryFilterComboBox.Location = new System.Drawing.Point(645, 547);
+            this.categoryFilterComboBox.Name = "categoryFilterComboBox";
+            this.categoryFilterComboBox.Size = new System.Drawing.Size(227, 33);
+            this.categoryFilterComboBox.TabIndex = 26;
+            this.categoryFilterComboBox.Click += new System.EventHandler(this.filterComboBox_SelectedIndexChanged);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(645, 375);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(227, 38);
+            this.searchTextBox.TabIndex = 25;
+            // 
+            // contentTextBox
+            // 
+            this.contentTextBox.Location = new System.Drawing.Point(0, 39);
+            this.contentTextBox.Multiline = true;
+            this.contentTextBox.Name = "contentTextBox";
+            this.contentTextBox.Size = new System.Drawing.Size(407, 717);
+            this.contentTextBox.TabIndex = 24;
+            // 
+            // notesListView
+            // 
+            this.notesListView.HideSelection = false;
+            this.notesListView.Location = new System.Drawing.Point(645, 39);
+            this.notesListView.MultiSelect = false;
+            this.notesListView.Name = "notesListView";
+            this.notesListView.Size = new System.Drawing.Size(227, 309);
+            this.notesListView.TabIndex = 23;
+            this.notesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(410, 76);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(173, 33);
+            this.categoryComboBox.TabIndex = 22;
+            this.categoryComboBox.SelectedIndexChanged += new System.EventHandler(this.categoryComboBox_SelectedIndexChanged);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(410, 0);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(175, 70);
+            this.addButton.TabIndex = 21;
+            this.addButton.Text = "Dodaj Notatkę";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(0, 0);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(407, 31);
+            this.titleTextBox.TabIndex = 20;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(897, 401);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(377, 116);
+            this.saveButton.TabIndex = 30;
+            this.saveButton.Text = "Zapisz";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(897, 545);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(377, 130);
+            this.openFileButton.TabIndex = 31;
+            this.openFileButton.Text = "Wczytaj";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // filterButton
+            // 
+            this.filterButton.Location = new System.Drawing.Point(645, 586);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(227, 89);
+            this.filterButton.TabIndex = 32;
+            this.filterButton.Text = "Filtruj";
+            this.filterButton.UseVisualStyleBackColor = true;
+            this.filterButton.Click += new System.EventHandler(this.filterButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(640, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Lista Notatek";
+            // 
+            // DeleteNoteButton
+            // 
+            this.DeleteNoteButton.Location = new System.Drawing.Point(897, 234);
+            this.DeleteNoteButton.Name = "DeleteNoteButton";
+            this.DeleteNoteButton.Size = new System.Drawing.Size(377, 114);
+            this.DeleteNoteButton.TabIndex = 34;
+            this.DeleteNoteButton.Text = "Usuń Notatkę";
+            this.DeleteNoteButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 755);
+            this.Controls.Add(this.DeleteNoteButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.reminderDateTimePicker);
+            this.Controls.Add(this.setReminderButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.categoryFilterComboBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.contentTextBox);
+            this.Controls.Add(this.notesListView);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.titleTextBox);
+            this.Name = "Form1";
+            this.Text = "NotesManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker reminderDateTimePicker;
+        private System.Windows.Forms.Button setReminderButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ComboBox categoryFilterComboBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.TextBox contentTextBox;
+        private System.Windows.Forms.ListView notesListView;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button filterButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DeleteNoteButton;
+    }
+}
+
---
+++ NotesManager/NotesManager/Form1.cs
@@ -0,0 +1,480 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotesManager
+{
+    public partial class Form1 : Form
+    {
+        private List<Note> notes = new List<Note>();
+        private List<Category> categories = new List<Category>();
+
+        private const string PlaceholderTextTitle = "Tytuł:";
+        private const string PlaceholderTextContent = "Notatka:";
+        private const string PlaceholderTextSearch = "Szukaj:";
+
+        private System.Windows.Forms.Timer reminderTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCategories();
+            InitializeDeleteNoteButton();
+            InitializeTextBox(titleTextBox, PlaceholderTextTitle);
+            InitializeTextBox(contentTextBox, PlaceholderTextContent);
+            InitializeTextBox(searchTextBox, PlaceholderTextSearch);
+
+
+            notesListView.ItemSelectionChanged += new ListViewItemSelectionChangedEventHandler(notesListView_ItemSelectionChanged);
+            reminderTimer = new System.Windows.Forms.Timer();
+            reminderTimer.Tick += new EventHandler(reminderTimer_Tick);
+            reminderTimer.Interval = 1000; 
+            reminderTimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            InitializeCategoryFilterComboBox();
+        }
+
+        private void InitializeTextBox(TextBox textBox, string placeholderText)
+        {
+           
+            textBox.Text = placeholderText;
+            textBox.ForeColor = SystemColors.GrayText; 
+
+           
+            textBox.Enter += TextBox_Enter;
+            textBox.Leave += TextBox_Leave;
+        }
+
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+           
+            if (textBox.Text == GetPlaceholderText(textBox))
+            {
+                textBox.Text = "";
+                textBox.ForeColor = SystemColors.WindowText; 
+            }
+        }
+
+        private void TextBox_Leave(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = GetPlaceholderText(textBox);
+                textBox.ForeColor = SystemColors.GrayText; 
+            }
+        }
+
+        private string GetPlaceholderText(TextBox textBox)
+        {
+            if (textBox == titleTextBox)
+            {
+                return PlaceholderTextTitle;
+            }
+            else if (textBox == contentTextBox)
+            {
+                return PlaceholderTextContent;
+            }
+            else if (textBox == searchTextBox)
+            {
+                return PlaceholderTextSearch;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private void InitializeCategoryFilterComboBox()
+        {
+            
+            categoryFilterComboBox.Items.Add("Wszystkie");
+
+            
+            foreach (Category category in categories)
+            {
+                categoryFilterComboBox.Items.Add(category.Name);
+            }
+
+            
+            categoryFilterComboBox.SelectedIndex = 0;
+        }
+        private void InitializeCategories()
+        {
+           
+            categories.Add(new Category("Praca", Color.Yellow));
+            categories.Add(new Category("Dom", Color.Green));
+            categories.Add(new Category("Osobiste", Color.Blue));
+            categories.Add(new Category("Szkoła", Color.Red));
+            categories.Add(new Category("Zakupy", Color.Purple));
+            categories.Add(new Category("Lista", Color.Brown));
+            categories.Add(new Category("Przypomnienia", Color.Black));
+
+            
+            foreach (Category category in categories)
+            {
+                categoryComboBox.Items.Add(category.Name);
+            }
+
+            
+            if (categoryComboBox.Items.Count > 0)
+            {
+                categoryComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+            string title = titleTextBox.Text;
+            string content = contentTextBox.Text;
+            DateTime creationDate = DateTime.Now;
+            Category selectedCategory = categories[categoryComboBox.SelectedIndex];
+
+            Note newNote = new Note(title, content, creationDate, selectedCategory);
+            notes.Add(newNote);
+
+            if (notesListView.Items.Count > 0)
+            {
+                notesListView.Items[notesListView.Items.Count - 1].Selected = true;
+            }
+
+            ListViewItem item = new ListViewItem(new string[] { title, content, creationDate.ToString(), selectedCategory.Name });
+            item.Tag = newNote; 
+            notesListView.Items.Add(item);
+
+            
+            titleTextBox.Clear();
+            contentTextBox.Clear();
+        }
+
+        private void notesListView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            DisplaySelectedNoteContent();
+        }
+
+        private void DisplaySelectedNoteContent()
+        {
+            if (notesListView.SelectedItems.Count > 0)
+            {
+                Note selectedNote = notesListView.SelectedItems[0].Tag as Note;
+
+                
+                contentTextBox.Text = selectedNote.Content;
+            }
+        }
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            
+            string searchTerm = searchTextBox.Text.ToLower();
+
+            notesListView.Items.Clear();
+
+            foreach (Note note in notes)
+            {
+                if (note.Title.ToLower().Contains(searchTerm) || note.Content.ToLower().Contains(searchTerm))
+                {
+                    ListViewItem item = new ListViewItem(new string[] { note.Title, note.Content, note.CreationDate.ToString(), note.Category.Name });
+                    item.Tag = note;
+                    notesListView.Items.Add(item);
+                }
+            }
+        }
+
+        private void filterComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            string selectedCategoryName = categoryFilterComboBox.SelectedItem as string;
+
+            notesListView.Items.Clear();
+
+            foreach (Note note in notes)
+            {
+                if (note.Category.Name == selectedCategoryName || selectedCategoryName == "Wszystkie")
+                {
+                    ListViewItem item = new ListViewItem(new string[] { note.Title, note.Content, note.CreationDate.ToString(), note.Category.Name });
+                    item.Tag = note;
+                    notesListView.Items.Add(item);
+                }
+            }
+        }
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            FilterNotes();
+        }
+
+        private void FilterNotes()
+        {
+            
+            string selectedCategoryName = categoryFilterComboBox.SelectedItem as string;
+
+            notesListView.Items.Clear();
+
+            foreach (Note note in notes)
+            {
+                if (selectedCategoryName == "Wszystkie" || note.Category.Name == selectedCategoryName)
+                {
+                    ListViewItem item = new ListViewItem(new string[] { note.Title, note.Content, note.CreationDate.ToString(), note.Category.Name });
+                    item.Tag = note;
+                    notesListView.Items.Add(item);
+                }
+            }
+        }
+        private void SaveNotesToFile(string filePath)
+        {
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        foreach (Note note in notes)
+                        {
+                            writer.WriteLine($"{note.Title},{note.Content},{note.CreationDate},{note.Category.Name}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd podczas zapisywania do pliku: {ex.Message}");
+                }
+            }
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                SaveNotesToFile(filePath);
+            }
+        }
+        private List<Note> LoadNotesFromFile(string filePath)
+        {
+            List<Note> loadedNotes = new List<Note>();
+
+            try
+            {
+                
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        
+                        Note loadedNote = ParseLineAndCreateNote(line);
+                        if (loadedNote != null)
+                        {
+                            loadedNotes.Add(loadedNote);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wystąpił błąd podczas wczytywania pliku: {ex.Message}");
+            }
+
+            return loadedNotes;
+        }
+        private Note ParseLineAndCreateNote(string line)
+        {
+            try
+            {
+                
+                string[] parts = line.Split(',');
+
+                if (parts.Length >= 4)
+                {
+                    string title = parts[0].Trim();
+                    string content = parts[1].Trim();
+                    DateTime creationDate = DateTime.Parse(parts[2].Trim());
+                    string categoryName = parts[3].Trim();
+
+                    
+                    Category category = categories.FirstOrDefault(c => c.Name == categoryName);
+
+                    if (category != null)
+                    {
+                        
+                        return new Note(title, content, creationDate, category);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd podczas przetwarzania linii: {ex.Message}");
+            }
+
+            
+            return null;
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+            openFileDialog.Title = "Wybierz plik tekstowy do wczytania";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                
+                List<Note> loadedNotes = LoadNotesFromFile(filePath);
+
+                
+                notes.AddRange(loadedNotes);
+
+                
+                foreach (Note loadedNote in loadedNotes)
+                {
+                    ListViewItem item = new ListViewItem(new string[] { loadedNote.Title, loadedNote.Content, loadedNote.CreationDate.ToString(), loadedNote.Category.Name });
+                    item.Tag = loadedNote;
+                    notesListView.Items.Add(item);
+                }
+
+                MessageBox.Show("Pomyślnie wczytano notatki z pliku.");
+            }
+        }
+        private void InitializeDeleteNoteButton()
+        {
+            DeleteNoteButton.Click += DeleteNoteButton_Click;
+        }
+
+        private void DeleteNoteButton_Click(object sender, EventArgs e)
+        {
+            if (notesListView.SelectedItems.Count > 0)
+            {
+                Note selectedNote = notesListView.SelectedItems[0].Tag as Note;
+
+                
+                categoryComboBox.Items.Remove(selectedNote.Title);
+                notes.Remove(selectedNote);
+
+                
+                DisplayNotes();
+            }
+            else
+            {
+                MessageBox.Show("Proszę wybrać notatkę przed usunięciem.");
+            }
+        }
+        private void DisplayNotes()
+        {
+            
+            notesListView.Items.Clear();
+
+            foreach (Note note in notes)
+            {
+                ListViewItem item = new ListViewItem(new string[] { note.Title, note.Content, note.CreationDate.ToString(), note.Category.Name });
+                item.Tag = note;
+                notesListView.Items.Add(item);
+            }
+        }
+        private void setReminderButton_Click(object sender, EventArgs e)
+        {
+           
+            if (notesListView.SelectedItems.Count > 0)
+            {
+                Note selectedNote = notesListView.SelectedItems[0].Tag as Note;
+
+                
+                DateTime reminderDateTime = reminderDateTimePicker.Value;
+
+                
+                selectedNote.ReminderDateTime = reminderDateTime;
+
+                
+                MessageBox.Show($"Ustawiono przypomnienie dla notatki '{selectedNote.Title}' na {reminderDateTime}");
+            }
+            else
+            {
+                MessageBox.Show("Proszę wybrać notatkę przed ustawieniem przypomnienia.");
+            }
+        }
+
+        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reminderDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            reminderDateTimePicker.Format = DateTimePickerFormat.Custom;
+            reminderDateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm";
+        }
+        private async void reminderTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Note note in notes)
+            {
+                if (!note.IsReminderDisplayed && note.ReminderDateTime != DateTime.MinValue && DateTime.Now.ToUniversalTime() >= note.ReminderDateTime.ToUniversalTime())
+                {
+
+                    reminderTimer.Stop();
+
+                   
+                    MessageBox.Show($"Przypomnienie dla notatki '{note.Title}'!");
+
+                   
+                    note.IsReminderDisplayed = true;
+
+                   
+                    await Task.Delay(5000); 
+
+                    reminderTimer.Start();
+                }
+            }
+        }
+    }
+
+    public class Note
+    {
+        public string Title { get; }
+        public string Content { get; }
+        public DateTime CreationDate { get; }
+        public Category Category { get; }
+        public DateTime ReminderDateTime { get; set; }
+        public bool IsReminderDisplayed { get; set; }
+
+        public Note(string title, string content, DateTime creationDate, Category category)
+        {
+            Title = title;
+            Content = content;
+            CreationDate = creationDate;
+            Category = category;
+        }
+    }
+
+    public class Category
+    {
+        public string Name { get; }
+        public Color Color { get; }
+
+        public Category(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+}
+    
+
